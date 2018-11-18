6d5cafdf0eb97b3b20e33cd792366056b076b7f9
Bodya <onlysnoopy1707@gmail.com>
2018-11-18 21:03:33 +0300

    Added Service and use in Console

--- TestProg/ConsoleClient/HttpService.cs
+++ TestProg/ConsoleClient/HttpService.cs
@@ -1,16 +1,42 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ConsoleClient
 {
-    class User
-    {
-
-    }
-    class HttpService
+    public class HttpService
     {
         //Users CRUD GetUserData GetListUsers  
-        
+        List<User> users = new List<User>();
+
+        public  User GetUser (int id )
+        {
+            var user = users.FirstOrDefault(x => x.ID == id);
+            return user;
+        }
+        public List<User> GetListUser()
+        {
+            return users;
+        }
+        public void Create(User user)
+        {
+            users.Add(user);
+            
+        }
+        public void Update(User user)
+        {
+            var userfind = users.FirstOrDefault(x => x.ID == user.ID);
+            userfind.Age = user.Age;
+            userfind.First_Name = user.First_Name;
+            userfind.Last_Name = user.Last_Name;
+            userfind.Mail = user.Mail;
+        }
+        public void Delete(int id)
+        {
+            var user = users.FirstOrDefault(x => x.ID == id);
+            users.Remove(user);
+        }
     }
 }
--- TestProg/ConsoleClient/Program.cs
+++ TestProg/ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain;
+using System;
 
 namespace ConsoleClient
 {
@@ -6,9 +7,82 @@ namespace ConsoleClient
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Hello World!");
+            HttpService service = new HttpService();
             
+
+            while (true)
+            {
+                Console.WriteLine("Change event: add, update, delete, viewuser, viewuserslist");
+                var userstring = Console.ReadLine().ToLower();
+                switch (userstring)
+                {
+                    case "viewuserslist":
+                        {
+                            var listusers = service.GetListUser();
+                            foreach (var item in listusers)
+                            {
+                                Console.WriteLine(item.First_Name);
+                                Console.WriteLine(item.Last_Name);
+                                Console.WriteLine(item.Age);
+                                Console.WriteLine(item.Mail);
+                            }
+                        }
+                        break;
+                    case "viewuser":
+                        {
+                            Console.WriteLine("Enter user ID");
+                            var id = Convert.ToInt32(Console.ReadLine());
+                            var user = service.GetUser(id);
+                            Console.WriteLine(user.First_Name);
+                            Console.WriteLine(user.Last_Name);
+                            Console.WriteLine(user.Age);
+                            Console.WriteLine(user.Mail);
+                        }
+                        break;
+                    case "add":
+                        {
+
+                            Console.WriteLine("Enter Name");
+                            var name = Console.ReadLine();
+                            Console.WriteLine("Enter Last Name");
+                            var lastname = Console.ReadLine();
+                            Console.WriteLine("Enter Age");
+                            var age = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter Mail");
+                            var mail = Console.ReadLine();
+                            User user = new User() { First_Name = name, Last_Name = lastname, Age = age, Mail = mail };
+                            service.Create(user);
+                            Console.WriteLine("User success added");
+                        }
+                        break;
+                    case "update":
+                        {
+                            Console.WriteLine("Enter Name");
+                            var name = Console.ReadLine();
+                            Console.WriteLine("Enter Last Name");
+                            var lastname = Console.ReadLine();
+                            Console.WriteLine("Enter Age");
+                            var age = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter Mail");
+                            var mail = Console.ReadLine();
+                            User user = new User() { First_Name = name, Last_Name = lastname, Age = age, Mail = mail };
+                            service.Update(user);
+                            Console.WriteLine("User success updated");
+                        }
+                        break;
+                    case "delete":
+                        {
+                            Console.WriteLine("Enter user ID");
+                            var id = Convert.ToInt32(Console.ReadLine());
+                            service.Delete(id);
+                            Console.WriteLine("User success deleted");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Wrong enter");
+                        break;
+                }
+            }
         }
     }
 }
