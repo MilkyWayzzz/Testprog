f20dba7e8c90e62858bf0ce1c76f58e37af646c9
Unknown <vladgq@gmail.com>
2018-11-18 21:04:31 +0300

    added api user service and database

---
+++ TestProg/ServerAPI/Controllers/UsersController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServerAPI.Models;
+
+namespace ServerAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserContext _userContext;
+
+        public UsersController(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        // GET api/users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {
+            var users = await _userContext.Users.ToListAsync();
+
+            return users;
+        }
+
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> Get(int id)
+        {
+            var user = await _userContext.Users.FirstOrDefaultAsync(x=>x.Id == id);
+
+            if (user == null)
+                return BadRequest();
+
+            return user;
+        }
+
+        // POST api/users
+        [HttpPost]
+        public async Task Post([FromBody] User user)
+        {
+            if (user == null || user.FirstName == null || user.LastName == null || user.Age == 0 || user.Email == null)
+                return;
+
+            _userContext.Users.Add(user);
+
+            await _userContext.SaveChangesAsync();
+        }
+
+
+        // PUT api/users/5
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody] User user)
+        {
+            if (id == 0 || user == null || user.FirstName == null || user.LastName == null || user.Age == 0 || user.Email == null)
+                return;
+
+            var userDb = await _userContext.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (userDb == null)
+                return;
+
+            userDb.FirstName = user.FirstName;
+            userDb.LastName = user.LastName;
+            userDb.Email = user.Email;
+            userDb.Age = user.Age;
+
+            await _userContext.SaveChangesAsync();
+        }
+
+        // DELETE api/users/5
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            var userDb = await _userContext.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (userDb == null)
+                return;
+
+            _userContext.Users.Remove(userDb);
+
+            await _userContext.SaveChangesAsync();
+        }
+    }
+}
--- TestProg/ServerAPI/Startup.cs
+++ TestProg/ServerAPI/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ServerAPI.Models;
 
 namespace ServerAPI
 {
@@ -25,6 +27,10 @@ namespace ServerAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<UserContext>(options =>
+                options.UseSqlServer(connection));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
